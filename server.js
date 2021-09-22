35b1a05e8bba9824ea6850e0d177cbc8792a5826
Nucleoo <neeraj26pathak@gmail.com>
2021-09-22 22:55:15 +0530

    Second commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+// Initialize Express
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
+// Connect Database
+connectDB();
+
+// Routes
 app.get('/', (req, res) => res.send('Server Running'));
 
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+// Listen to Server
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
