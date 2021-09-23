225c1cbfd379f53a6bd5986618240c2c72212222
Nucleoo <neeraj26pathak@gmail.com>
2021-09-23 12:16:42 +0530

    Authenticated

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));  // Similar as body parser
+
 // Routes
 app.get('/', (req, res) => res.send('Server Running'));
 
